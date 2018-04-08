d5987d3baed9592425c4bee9ba9e6b16be20990e
filip9696 <filip@kinmails.com>
2018-04-07 20:03:28 -0400

    fixed social icons mostly

--- resume.js
+++ resume.js
@@ -45,52 +45,6 @@ $(document).ready(function () {
     });
 });
 
-function phoneClick() {
-    let discord = document.getElementById('discord-social');
-    if (discord.innerHTML === "Filip96#8066") {
-        discord.innerHTML = '<i class="fab fa-discord"></i>';
-        discord.style.marginTop = "0px";
-        discord.style.fontSize = "40px";
-        discord.style.width = "90px";
-    }
-    let phone = document.getElementById('phone-social');
-    if (phone.innerHTML === "248-946-6028") {
-        phone.style.marginTop = "0px";
-        phone.style.width = "90px";
-        phone.innerHTML = '<i class="fas fa-phone"></i>';
-        phone.style.fontSize = "40px"
-    } else {
-        phone.innerHTML = '248-946-6028';
-        phone.classList.remove("social-icon");
-        phone.style.fontSize = "27px";
-        phone.style.marginTop = "0px";
-        phone.style.width = "210px"
-    }
-}
-
-function discordClick() {
-    let phone = document.getElementById('phone-social');
-    if (phone.innerHTML === "248-946-6028") {
-        phone.innerHTML = '<i class="fas fa-phone"></i>';
-        phone.style.marginTop = "0px";
-        phone.style.fontSize = "40px";
-        phone.style.width = "90px";
-    }
-    let discord = document.getElementById('discord-social');
-    if (discord.innerHTML === "Filip96#8066") {
-        discord.style.marginTop = "0px";
-        discord.style.width = "90px";
-        discord.innerHTML = '<i class="fab fa-discord"></i>';
-        discord.style.fontSize = "40px"
-    } else {
-        discord.innerHTML = 'Filip96#8066';
-        discord.classList.remove("social-icon");
-        discord.style.fontSize = "27px";
-        discord.style.marginTop = "0px";
-        discord.style.width = "195px";
-    }
-}
-
 var projcodes = {
     'proj-ed': ['Early Development', 'light-blue-text'],
     'proj-mdh': ['Mid-Development Hell', 'red-text'],
